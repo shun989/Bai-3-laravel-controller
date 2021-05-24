f3ad2de0d3be7ac577c6bcbe90c69f696fe12809
shun989 <minhtuanvt89@gmail.com>
2021-05-24 13:17:53 +0700

    Ứng dụng kiểm tra email hợp lệ

--- checkEmail/routes/web.php
+++ checkEmail/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::post('check_email', [UserController::class, 'validationEmail'])->name('checkEmail');
